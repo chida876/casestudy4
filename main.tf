e4ceef52411bfec6e0a2bc00fd5222edbad27545
chida876 <116781445+chida876@users.noreply.github.com>
2024-04-19 08:57:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,86 +3,53 @@ provider "aws" {
 }
 
 # Create VPC
-resource "aws_vpc" "main" {
+resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "main-vpc"
-  }
 }
 
-# Create Subnets in three different availability zones
+# Create subnets in different availability zones
 resource "aws_subnet" "subnet1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
 }
 
 resource "aws_subnet" "subnet2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
   availability_zone = "ap-south-1b"
 }
 
 resource "aws_subnet" "subnet3" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.3.0/24"
   availability_zone = "ap-south-1c"
 }
 
-# Create S3 Bucket
-resource "aws_s3_bucket" "shared_storage" {
-  bucket = "shared-bucket-876-${timestamp()}"
-  acl    = "private"
+# Create an S3 bucket
+resource "aws_s3_bucket" "my_shared_bucket" {
+  bucket = "my-shared-bucket"
 }
 
-resource "aws_s3_bucket_acl" "shared_storage_acl" {
-  bucket = aws_s3_bucket.shared_storage.bucket
-  acl    = "private"
-}
-
-# EC2 Instances
+# EC2 instances
 resource "aws_instance" "ec2_instance" {
   count = 3
 
-  ami           = "ami-007020fd9c84e18c7"
+  ami           = "ami-007020fd9c84e18c7" # Ubuntu 20.04 LTS
   instance_type = "t2.micro"
-  subnet_id     = element(aws_subnet.subnet1.*.id, count.index % 3)
-  key_name      = "springboot-1"
+  key_name      = "keypair"
+  iam_instance_profile = "s3_access_role" # Replace with your IAM role name
+  
+  subnet_id = count.index == 0 ? aws_subnet.subnet1.id : count.index == 1 ? aws_subnet.subnet2.id : aws_subnet.subnet3.id
 
   user_data = <<-EOF
               #!/bin/bash
-              yum update -y
-              yum install -y docker
-              systemctl start docker
-              systemctl enable docker
-              echo 'docker run -v /mnt/s3:/data -d -p 80:80 nginx' >> /etc/rc.local
-              mkdir /mnt/s3
-              mount -t fuse.s3fs ${aws_s3_bucket.shared_storage.bucket} /mnt/s3 -o passwd_file=/etc/passwd-s3fs -o url=https://s3.amazonaws.com -o allow_other
+              sudo apt-get update
+              sudo apt-get install -y docker.io
+              sudo apt-get install -y s3fs
+              echo 'access_key:secret_key' > /home/ubuntu/.passwd-s3fs
+              chmod 600 /home/ubuntu/.passwd-s3fs
+              sudo mkdir /mnt/my_shared_bucket
+              sudo s3fs ${aws_s3_bucket.my_shared_bucket.bucket} /mnt/my_shared_bucket -o passwd_file=/home/ubuntu/.passwd-s3fs -o url=https://s3.amazonaws.com -o use_path_request_style
               EOF
-
-  tags = {
-    Name = "ec2-instance-${count.index}"
-  }
-}
-
-# S3 Bucket Policy
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.shared_storage.bucket
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:*",
-        Resource  = [aws_s3_bucket.shared_storage.arn, "${aws_s3_bucket.shared_storage.arn}/*"],
-        Condition = {
-          StringEquals = {
-            "aws:SourceVpce" = "*"
-          }
-        }
-      }
-    ]
-  })
 }
